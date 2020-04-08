b471222d8168c80ebacb6e6aaeb4f4b7dce16414
krawcox <krawcox123@gmail.com>
2020-04-08 14:03:55 +0200

    Last correction. I moved initializing hero to create function and deleted additional update function calls

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,16 +7,17 @@ import flixel.text.FlxText;
 class PlayState extends FlxState
 {
 	public var tekst:FlxText = new FlxText("hello there");
-	public var bohater:Hero = new Hero(25, 25);
-	public var bohater_1:Hero = new Hero(10, 23);
-	public var bohater_2:Hero = new Hero(6, 2);
-	public var bohater_3:Hero = new Hero(10, 10);
-	public var bohater_4:Hero = new Hero(2, 27);
-	public var bohater_5:Hero = new Hero(50, 15);
-	public var bohater_6:Hero = new Hero(14, 7);
+
 	override public function create()
 	{
 		super.create();
+		var bohater:Hero = new Hero(25, 25);
+		var bohater_1:Hero = new Hero(10, 23);
+		var bohater_2:Hero = new Hero(6, 2);
+		var bohater_3:Hero = new Hero(10, 10);
+		var bohater_4:Hero = new Hero(2, 27);
+		var bohater_5:Hero = new Hero(50, 15);
+		var bohater_6:Hero = new Hero(14, 7);
 		var wyglad:FlxTextFormat = new FlxTextFormat(0xFF00FFFF, true, true, 0x00000000);
 		tekst.addFormat(wyglad);
 		add(tekst); 
@@ -33,13 +34,6 @@ class PlayState extends FlxState
 	{
 		tekst.x += 2;
 		tekst.y += 1;
-		bohater.update(elapsed);
-		bohater_1.update(elapsed);
-		bohater_2.update(elapsed);
-		bohater_3.update(elapsed);
-		bohater_4.update(elapsed);
-		bohater_5.update(elapsed);
-		bohater_6.update(elapsed);
 		super.update(elapsed);
 		
 	}
